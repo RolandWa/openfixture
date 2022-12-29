46b786eac6882ddc873fdb1bd2d9d84ba14bb05a
Roland <roland.wache@rockwellautomation.com>
2022-12-29 11:33:06 +0100

    Update bat file to run proper under Windows environment

--- genfixture.bat
+++ genfixture.bat
@@ -6,11 +6,11 @@ TITLE OpenSCAD fixture generator
 setlocal
 
 REM Patch to openscad
-IF EXIST "C:\Program Files\OpenSCAD" SET PATH=%PATH%;"C:\Program Files\OpenSCAD\"
+IF EXIST "C:\Program Files\OpenSCAD" SET PATH=%PATH%C:\Program Files\OpenSCAD\;
 
 REM Patch to KiCad
-IF EXIST "C:\Program Files\KiCad\bin" SET KICAD = "C:\Program Files\KiCad\bin\"
-REM IF EXIST "C:\Program Files\KiCad\6.0\bin" SET KICAD = "C:\Program Files\KiCad\6.0\bin\"
+IF EXIST "C:\Program Files\KiCad\bin" SET KICAD=C:\Program Files\KiCad\bin
+IF EXIST "C:\Program Files\KiCad\6.0\bin" SET KICAD=C:\Program Files\KiCad\6.0\bin
 
 set BOARD=%1
 
@@ -34,4 +34,4 @@ set NUT_C2C=6.10
 set POGO_UNCOMPRESSED_LENGTH=16
 
 ECHO "KiCAD Location : " %KICAD%
-"C:\Program Files\KiCad\bin\python.exe" GenFixture.py --board %BOARD% --layer %LAYER% --rev %REV% --mat_th %MAT% --pcb_th %PCB% --out %OUTPUT% --screw_len %SCREW_LEN% --screw_d %SCREW_D% --washer_th %WASHER_TH% --nut_th %NUT_TH% --nut_f2f %NUT_F2F% --nut_c2c %NUT_C2C% --border %BORDER% --pogo-uncompressed-length %POGO_UNCOMPRESSED_LENGTH%
+"%KICAD%\python.exe" GenFixture.py --board %BOARD% --layer %LAYER% --rev %REV% --mat_th %MAT% --pcb_th %PCB% --out %OUTPUT% --screw_len %SCREW_LEN% --screw_d %SCREW_D% --washer_th %WASHER_TH% --nut_th %NUT_TH% --nut_f2f %NUT_F2F% --nut_c2c %NUT_C2C% --border %BORDER% --pogo-uncompressed-length %POGO_UNCOMPRESSED_LENGTH%
